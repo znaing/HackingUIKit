2785a35bec7fdab44814eb7e89e4aa445df3b526
znaing <zaidpyinoolwin@gmail.com>
2023-10-30 23:48:33 -0700

    Add Bar Button triggers an alert that populateds the table view

--- Project5/Project5/ViewController.swift
+++ Project5/Project5/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UITableViewController {
     var usedWords = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
         // Do any additional setup after loading the view.
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt"){
             if let startWords = try? String(contentsOf: startWordsURL){
@@ -36,7 +37,38 @@ class ViewController: UITableViewController {
         cell.textLabel?.text = usedWords[indexPath.row]
         return cell
     }
+    @objc func promptForAnswer(){
+        let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        let submitAction = UIAlertAction(title: "Submit", style: .default) {
+            [weak self, weak ac ] _ in
+            guard let answer = ac?.textFields?[0].text else { return }
+            self?.submit(answer)
+        }
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
     
-
+    func submit(_ answer: String ){
+        var lowercase = answer.lowercased()
+        if isPossible(word: lowercase){
+            if isOriginal(word: lowercase){
+                if isReal(word: lowercase){
+                    usedWords.insert(answer, at: 0)
+                    let indexPath = IndexPath(row: 0, section: 0)
+                    tableView.insertRows(at: [indexPath], with: .automatic)
+                }
+            }
+        }
+    }
+    func isPossible(word: String)-> Bool{
+        return true
+    }
+    func isOriginal(word: String)-> Bool{
+        return true
+    }
+    func isReal(word: String)-> Bool{
+        return true
+    }
 }
 
